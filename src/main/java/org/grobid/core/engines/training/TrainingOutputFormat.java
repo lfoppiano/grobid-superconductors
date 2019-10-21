183ab4d514eef7eebb625c5d28e1d6668bcbc801
Luca Foppiano <luca@foppiano.org>
2019-10-21 15:15:03 +0900

    ouptut xml files with extension tei.xml

--- src/main/java/org/grobid/core/engines/training/TrainingOutputFormat.java
+++ src/main/java/org/grobid/core/engines/training/TrainingOutputFormat.java
@@ -1,6 +1,17 @@
 package org.grobid.core.engines.training;
 
 public enum TrainingOutputFormat {
-    TSV,
-    XML
+    TSV("tsv"),
+    XML("tei.xml");
+
+    private String name;
+
+    TrainingOutputFormat(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
