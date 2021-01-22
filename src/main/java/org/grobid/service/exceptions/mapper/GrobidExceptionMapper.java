5a676b2e3b8eabd7793a634aa55262530a5f2c5c
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2021-01-22 15:24:14 +0900

    log exceptions before sending them back to the controller

--- src/main/java/org/grobid/service/exceptions/mapper/GrobidExceptionMapper.java
+++ src/main/java/org/grobid/service/exceptions/mapper/GrobidExceptionMapper.java
@@ -1,7 +1,10 @@
 package org.grobid.service.exceptions.mapper;
 
 import com.google.inject.Inject;
+import org.grobid.core.engines.SuperconductorsParser;
 import org.grobid.core.exceptions.GrobidException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
@@ -12,6 +15,7 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 public class GrobidExceptionMapper implements ExceptionMapper<GrobidException> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GrobidExceptionMapper.class);
 
     @Context
     protected HttpHeaders headers;
@@ -30,6 +34,7 @@ public class GrobidExceptionMapper implements ExceptionMapper<GrobidException> {
 
     @Override
     public Response toResponse(GrobidException exception) {
+        LOGGER.error("An exception has occurred:", exception);
         return mapper.processException(exception, GrobidStatusToHttpStatusMapper.getStatusCode(exception.getStatus()));
     }
 }
