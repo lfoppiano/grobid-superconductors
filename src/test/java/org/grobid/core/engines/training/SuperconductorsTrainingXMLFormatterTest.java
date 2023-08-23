0a900d5b6002d99546b2673f4fe9db9e5ce6fbf8
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2023-08-23 14:33:58 +0900

    fix tests

--- src/test/java/org/grobid/core/engines/training/SuperconductorsTrainingXMLFormatterTest.java
+++ src/test/java/org/grobid/core/engines/training/SuperconductorsTrainingXMLFormatterTest.java
@@ -6,10 +6,13 @@ import org.grobid.core.analyzers.DeepAnalyzer;
 import org.grobid.core.data.document.DocumentBlock;
 import org.grobid.core.data.document.Span;
 import org.grobid.core.layout.LayoutToken;
+import org.grobid.core.utilities.AdditionalLayoutTokensUtil;
+import org.grobid.core.utilities.OffsetPosition;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.grobid.core.document.xml.XmlBuilderUtils.teiElement;
@@ -123,13 +126,17 @@ public class SuperconductorsTrainingXMLFormatterTest {
     @Test
     public void testDocumentConstruction() {
         List<Span> spanListTitle = new ArrayList<>();
-        Span span1 = new Span("(TMTSF)2PF6", SUPERCONDUCTORS_MATERIAL_LABEL);
-        span1.setOffsetStart(19);
-        span1.setOffsetEnd(30);
-        spanListTitle.add(span1);
-
+        Span anotherSpan = new Span("(TMTSF)2PF6", SUPERCONDUCTORS_MATERIAL_LABEL);
+        anotherSpan.setOffsetStart(19);
+        anotherSpan.setOffsetEnd(30);
+        spanListTitle.add(anotherSpan);
+        
         String textTitle = "The Bechgaard salt (TMTSF)2PF6 (TMTSF = tetra- methyltetraselenafulvalene) was";
         List<LayoutToken> layoutTokensTitle = DeepAnalyzer.getInstance().tokenizeWithLayoutToken(textTitle);
+
+        Pair<Integer, Integer> indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokensTitle, 19, 30);
+        anotherSpan.setLayoutTokens(layoutTokensTitle.subList(indexes.getLeft(), indexes.getRight()));
+
 
         List<DocumentBlock> blocks = new ArrayList<>();
         DocumentBlock documentBlockTitle = new DocumentBlock(layoutTokensTitle, DocumentBlock.SECTION_HEADER, DocumentBlock.SUB_SECTION_TITLE, spanListTitle, new ArrayList<>());
@@ -153,54 +160,69 @@ public class SuperconductorsTrainingXMLFormatterTest {
         });
 
         List<Span> spanListParagraph = new ArrayList<>();
-        Span Span = new Span();
-        Span.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
-        Span.setOffsetStart(445);
-        Span.setOffsetEnd(458);
-        Span.setText("FeSe 1-x Te x");
-        spanListParagraph.add(Span);
+        Span span1 = new Span();
+        span1.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
+        span1.setOffsetStart(445);
+        span1.setOffsetEnd(458);
+        span1.setText("FeSe 1-x Te x");
+        spanListParagraph.add(span1);
+        indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokensParagraph, 445, 458);
+        span1.setLayoutTokens(layoutTokensParagraph.subList(indexes.getLeft(), indexes.getRight()));
 
-        Span Span2 = new Span();
-        Span2.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
-        Span2.setOffsetStart(460);
-        Span2.setOffsetEnd(471);
-        Span2.setText("0.6 ≤ x ≤ 1");
-        spanListParagraph.add(Span2);
 
-        Span Span3 = new Span();
-        Span3.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
-        Span3.setOffsetStart(549);
-        Span3.setOffsetEnd(561);
-        Span3.setText("x = 0.6 -0.9");
-        spanListParagraph.add(Span3);
+        Span span2 = new Span();
+        span2.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
+        span2.setOffsetStart(460);
+        span2.setOffsetEnd(471);
+        span2.setText("0.6 ≤ x ≤ 1");
+        spanListParagraph.add(span2);
+        indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokensParagraph, 460, 471);
+        span2.setLayoutTokens(layoutTokensParagraph.subList(indexes.getLeft(), indexes.getRight()));
 
-        Span Span4 = new Span();
-        Span4.setType(SUPERCONDUCTORS_TC_VALUE_LABEL);
-        Span4.setOffsetStart(562);
-        Span4.setOffsetEnd(569);
-        Span4.setText("exhibit");
-        spanListParagraph.add(Span4);
+        Span span3 = new Span();
+        span3.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
+        span3.setOffsetStart(549);
+        span3.setOffsetEnd(561);
+        span3.setText("x = 0.6 -0.9");
+        spanListParagraph.add(span3);
+        indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokensParagraph, 549, 561);
+        span3.setLayoutTokens(layoutTokensParagraph.subList(indexes.getLeft(), indexes.getRight()));
 
-        Span Span5 = new Span();
-        Span5.setType(SUPERCONDUCTORS_TC_LABEL);
-        Span5.setOffsetStart(570);
-        Span5.setOffsetEnd(592);
-        Span5.setText("bulk superconductivity");
-        spanListParagraph.add(Span5);
+        Span span4 = new Span();
+        span4.setType(SUPERCONDUCTORS_TC_VALUE_LABEL);
+        span4.setOffsetStart(562);
+        span4.setOffsetEnd(569);
+        span4.setText("exhibit");
+        spanListParagraph.add(span4);
+        indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokensParagraph, 562, 569);
+        span4.setLayoutTokens(layoutTokensParagraph.subList(indexes.getLeft(), indexes.getRight()));
 
-        Span Span6 = new Span();
-        Span6.setType(SUPERCONDUCTORS_TC_LABEL);
-        Span6.setOffsetStart(632);
-        Span6.setOffsetEnd(647);
-        Span6.setText("superconducting");
-        spanListParagraph.add(Span6);
+        Span span5 = new Span();
+        span5.setType(SUPERCONDUCTORS_TC_LABEL);
+        span5.setOffsetStart(570);
+        span5.setOffsetEnd(592);
+        span5.setText("bulk superconductivity");
+        spanListParagraph.add(span5);
+        indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokensParagraph, 570, 592);
+        span5.setLayoutTokens(layoutTokensParagraph.subList(indexes.getLeft(), indexes.getRight()));
 
-        Span Span7 = new Span();
-        Span7.setType(SUPERCONDUCTORS_TC_LABEL);
-        Span7.setOffsetStart(653);
-        Span7.setOffsetEnd(675);
-        Span7.setText("transition temperature");
-        spanListParagraph.add(Span7);
+        Span span6 = new Span();
+        span6.setType(SUPERCONDUCTORS_TC_LABEL);
+        span6.setOffsetStart(632);
+        span6.setOffsetEnd(647);
+        span6.setText("superconducting");
+        spanListParagraph.add(span6);
+        indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokensParagraph, 632, 647);
+        span6.setLayoutTokens(layoutTokensParagraph.subList(indexes.getLeft(), indexes.getRight()));
+
+        Span span7 = new Span();
+        span7.setType(SUPERCONDUCTORS_TC_LABEL);
+        span7.setOffsetStart(653);
+        span7.setOffsetEnd(675);
+        span7.setText("transition temperature");
+        spanListParagraph.add(span7);
+        indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokensParagraph, 653, 675);
+        span7.setLayoutTokens(layoutTokensParagraph.subList(indexes.getLeft(), indexes.getRight()));
 
         DocumentBlock blockParagraph = new DocumentBlock(layoutTokensParagraph, DocumentBlock.SECTION_BODY, DocumentBlock.SUB_SECTION_PARAGRAPH, spanListParagraph, new ArrayList<>());
         blocks.add(blockParagraph);
@@ -263,10 +285,13 @@ public class SuperconductorsTrainingXMLFormatterTest {
         span1.setText("(TMTSF)2PF6");
 
         String text = "The Bechgaard salt (TMTSF)2PF6 (TMTSF = tetra- methyltetraselenafulvalene) was";
+        List<LayoutToken> tokens = DeepAnalyzer.getInstance().tokenizeWithLayoutToken(text);
+        span1.setLayoutTokens(tokens.subList(6, 13));
+
 
         spanList.add(span1);
 
-        Element out = target.trainingExtraction(spanList, DeepAnalyzer.getInstance().tokenizeWithLayoutToken(text));
+        Element out = target.trainingExtraction(spanList, tokens);
         assertThat(out.toXML(), is("<p xmlns=\"http://www.tei-c.org/ns/1.0\">The Bechgaard salt <rs type=\"material\">(TMTSF)2PF6</rs> (TMTSF = tetra- methyltetraselenafulvalene) was</p>"));
     }
 
@@ -287,12 +312,14 @@ public class SuperconductorsTrainingXMLFormatterTest {
         superconductor.setOffsetStart(64);
         superconductor.setOffsetEnd(77);
         superconductor.setText("FeSe 1-x Te x");
+        superconductor.setLayoutTokens(layoutTokens.subList(18, 27));
 
         Span superconductor2 = new Span();
         superconductor2.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
         superconductor2.setOffsetStart(79);
         superconductor2.setOffsetEnd(90);
         superconductor2.setText("0.6 ≤ x ≤ 1");
+        superconductor2.setLayoutTokens(layoutTokens.subList(29, 40));
 
         superconductorList.add(superconductor);
         superconductorList.add(superconductor2);
@@ -330,12 +357,14 @@ public class SuperconductorsTrainingXMLFormatterTest {
         span1.setOffsetStart(64);
         span1.setOffsetEnd(77);
         span1.setText("FeSe 1-x Te x");
+        span1.setLayoutTokens(layoutTokens.subList(18, 27));
 
         Span span2 = new Span();
         span2.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
         span2.setOffsetStart(79);
         span2.setOffsetEnd(90);
         span2.setText("0.6 ≤ x ≤ 1");
+        span2.setLayoutTokens(layoutTokens.subList(29, 40));
 
         spanList.add(span1);
         spanList.add(span2);
@@ -366,54 +395,70 @@ public class SuperconductorsTrainingXMLFormatterTest {
         });
 
         List<Span> spanList = new ArrayList<>();
-        Span Span = new Span();
-        Span.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
-        Span.setOffsetStart(445);
-        Span.setOffsetEnd(458);
-        Span.setText("FeSe 1-x Te x");
-        spanList.add(Span);
+        Span span1 = new Span();
+        span1.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
+        span1.setOffsetStart(445);
+        span1.setOffsetEnd(458);
+        span1.setText("FeSe 1-x Te x");
+        spanList.add(span1);
+        Pair<Integer, Integer> indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokens, 445, 458);
+        span1.setLayoutTokens(layoutTokens.subList(indexes.getLeft(), indexes.getRight()));
+        
 
-        Span Span2 = new Span();
-        Span2.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
-        Span2.setOffsetStart(460);
-        Span2.setOffsetEnd(471);
-        Span2.setText("0.6 ≤ x ≤ 1");
-        spanList.add(Span2);
+        Span span2 = new Span();
+        span2.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
+        span2.setOffsetStart(460);
+        span2.setOffsetEnd(471);
+        span2.setText("0.6 ≤ x ≤ 1");
+        spanList.add(span2);
+        indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokens, 460, 471);
+        span2.setLayoutTokens(layoutTokens.subList(indexes.getLeft(), indexes.getRight()));
 
-        Span Span3 = new Span();
-        Span3.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
-        Span3.setOffsetStart(549);
-        Span3.setOffsetEnd(561);
-        Span3.setText("x = 0.6 -0.9");
-        spanList.add(Span3);
+        Span span3 = new Span();
+        span3.setType(SUPERCONDUCTORS_MATERIAL_LABEL);
+        span3.setOffsetStart(549);
+        span3.setOffsetEnd(561);
+        span3.setText("x = 0.6 -0.9");
+        spanList.add(span3);
+        indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokens, 549, 561);
+        span3.setLayoutTokens(layoutTokens.subList(indexes.getLeft(), indexes.getRight()));
 
-        Span Span4 = new Span();
-        Span4.setType(SUPERCONDUCTORS_TC_VALUE_LABEL);
-        Span4.setOffsetStart(562);
-        Span4.setOffsetEnd(569);
-        Span4.setText("exhibit");
-        spanList.add(Span4);
+        Span span4 = new Span();
+        span4.setType(SUPERCONDUCTORS_TC_VALUE_LABEL);
+        span4.setOffsetStart(562);
+        span4.setOffsetEnd(569);
+        span4.setText("exhibit");
+        spanList.add(span4);
+        indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokens, 562, 569);
+        span4.setLayoutTokens(layoutTokens.subList(indexes.getLeft(), indexes.getRight()));
 
-        Span Span5 = new Span();
-        Span5.setType(SUPERCONDUCTORS_TC_LABEL);
-        Span5.setOffsetStart(570);
-        Span5.setOffsetEnd(592);
-        Span5.setText("bulk superconductivity");
-        spanList.add(Span5);
+        Span span5 = new Span();
+        span5.setType(SUPERCONDUCTORS_TC_LABEL);
+        span5.setOffsetStart(570);
+        span5.setOffsetEnd(592);
+        span5.setText("bulk superconductivity");
+        spanList.add(span5);
+        indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokens, 570, 592);
+        span5.setLayoutTokens(layoutTokens.subList(indexes.getLeft(), indexes.getRight()));
 
-        Span Span6 = new Span();
-        Span6.setType(SUPERCONDUCTORS_TC_LABEL);
-        Span6.setOffsetStart(632);
-        Span6.setOffsetEnd(647);
-        Span6.setText("superconducting");
-        spanList.add(Span6);
+        Span span6 = new Span();
+        span6.setType(SUPERCONDUCTORS_TC_LABEL);
+        span6.setOffsetStart(632);
+        span6.setOffsetEnd(647);
+        span6.setText("superconducting");
+        spanList.add(span6);
+        indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokens, 632, 647);
+        span6.setLayoutTokens(layoutTokens.subList(indexes.getLeft(), indexes.getRight()));
 
-        Span Span7 = new Span();
-        Span7.setType(SUPERCONDUCTORS_TC_LABEL);
-        Span7.setOffsetStart(653);
-        Span7.setOffsetEnd(675);
-        Span7.setText("transition temperature");
-        spanList.add(Span7);
+        Span span7 = new Span();
+        span7.setType(SUPERCONDUCTORS_TC_LABEL);
+        span7.setOffsetStart(653);
+        span7.setOffsetEnd(675);
+        span7.setText("transition temperature");
+        spanList.add(span7);
+        indexes = AdditionalLayoutTokensUtil.getExtremitiesAsIndex(layoutTokens, 653, 675);
+        span7.setLayoutTokens(layoutTokens.subList(indexes.getLeft(), indexes.getRight()));
+        
 
         List<DocumentBlock> documentBlocks = new ArrayList<>();
         documentBlocks.add(new DocumentBlock(layoutTokens, DocumentBlock.SECTION_BODY, DocumentBlock.SUB_SECTION_PARAGRAPH, "1", spanList, new ArrayList<>()));
